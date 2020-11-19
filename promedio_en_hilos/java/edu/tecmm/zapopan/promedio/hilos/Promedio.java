3ef52c2dbfc41ded938c445161eae0bf1cf7f0bf
Miguel Bernal Marin <miguel.bernal@zapopan.tecmm.edu.mx>
2020-11-19 16:31:26 -0600

    java: iniciamos el programa promedio

---
+++ promedio_en_hilos/java/edu/tecmm/zapopan/promedio/hilos/Promedio.java
@@ -0,0 +1,26 @@
+/*
+ * SPDX-License-Identifier: MIT
+ */
+
+package edu.tecmm.zapopan.promedio.hilos;
+
+public class Promedio {
+
+    public static void main(String []args) {
+        System.out.println("Hola mundo");
+    }
+}
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 4
+ * tab-width: 4
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=4 tabstop=4 expandtab:
+ * :indentSize=4:tabSize=4:noTabs=true:
+ */
+
