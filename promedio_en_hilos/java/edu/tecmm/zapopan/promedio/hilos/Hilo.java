55c8d2b9a5043bec4ae4fd19b4385dbc821a81cf
Jorge Chavez <za17012072@zapopan.tecmm.edu.mx>
2020-12-03 23:18:31 -0600

    nueva clase hilo agregada

---
+++ promedio_en_hilos/java/edu/tecmm/zapopan/promedio/hilos/Hilo.java
@@ -0,0 +1,37 @@
+/*
+ * SPDX-License-Identifier: MIT
+ */
+
+package edu.tecmm.zapopan.promedio.hilos;
+
+
+
+public class Hilo extends Thread {
+
+    private int suma;
+
+    public void setSuma(int n) {
+        suma = n;
+    }
+
+    public int getSuma() {
+        return Suma;
+    }
+
+    @Override
+    public void run() {
+	
+    }
+}
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 4
+ * tab-width: 4
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=4 tabstop=4 expandtab:
+ * :indentSize=4:tabSize=4:noTabs=true:
+ */
